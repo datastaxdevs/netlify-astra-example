87706952054aa1b4f43b84692959951527fbed02
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-05-27 12:02:21 -0700

    Adding GQ stuff

--- src/utils/api.js
+++ src/utils/api.js
@@ -59,8 +59,13 @@ const getGQTodos = async () => {
 };
 
 const getRestTodos = async () => {
+  console.log('%cGetting Rest Todos', 'color: blue; font-size: x-large font-family: arial');
+  console.log('%c    GQ: GET /api/graphql/todos', 'color: blue; font-family: arial');
   const response = await fetch(`/.netlify/functions/getRestTodos`);
   let todos = await response.json()
+  console.log("where: " + 'where={"key":{"$eq":"rest"}}')
+
+  console.log('%c        ' + todos, 'color: blue; font-size: large font-family: arial')
   return todos.length ? todos : [];
 };
 
